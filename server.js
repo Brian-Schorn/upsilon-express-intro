33ce61f24c5b4c17bd00f8061c669892c537a553
Brian Schorn <schorn.brian@gmail.com>
2017-01-18 16:06:20 -0600

    Added conditionals to test for duplicate songs and blank fields

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var songs = require('./data.json');
 var app = express();
+var testTitle;
 
 //Makes everything in the public folder visible
 app.use(express.static('public'));
@@ -20,9 +21,38 @@ app.get('/songs', function(req, res) {
 });
 
 app.post('/songs', function(req, res){
-  console.log('req.body:', req.body);
-  songs.push(req.body);
-  res.sendStatus(200);
+  console.log('req.body:' + req.body);
+  // var x = req.body.title;
+  // console.log(x);
+  if(req.body.title != ""){
+    if(req.body.artist != ""){
+      if(req.body.album != ""){
+        console.log("Valid Entry");
+        testTitle = req.body.title;
+        if(songs.every(isDuplicate)){
+          console.log("Did not match any other song titles");
+          songs.push(req.body);
+          res.sendStatus(200);
+        }else{
+          console.log("Your entry appears to be a duplicate");
+          res.sendStatus(400);
+        }
+      }else{
+        console.log("Invalid Entry on Album");
+        res.sendStatus(400);
+      }
+    }else{
+      console.log("Invalid Entry on Artist");
+      res.sendStatus(400);
+    }
+  }else{
+    console.log("Invalid Entry on Title");
+    res.sendStatus(400);
+  }
 });
 
 app.listen(3000);
+
+function isDuplicate(element, index, array){
+  return element.title != testTitle;
+}
