8130d0e0e40f42dfb920638db07061fbcdd7d01c
Brian Schorn <schorn.brian@gmail.com>
2017-01-18 15:27:05 -0600

    Finished lecture

--- public/client.js
+++ public/client.js
@@ -1,14 +1,44 @@
 $(function(){
   console.log('document loaded');
 
-  $.ajax({
-    url: '/data',
-    type: 'GET',
-    success: displaySongs
-  });
+  getSongs();
+
+  $('#addSong').on('submit', addSong);
+
 
 });
 
+
+function addSong(event) {
+  //Stops browser from refreshing
+  event.preventDefault();
+
+  var songData = $(this).serialize();
+  console.log(songData);
+
+  $.ajax({
+    url: '/songs',
+    type: 'POST',
+    data: songData,
+    success: getSongs
+
+  });
+}
+
+function getSongs() {
+  $.ajax({
+    url: '/songs',
+    type: 'GET',
+    success: displaySongs
+  });
+}
+
+
 function displaySongs(songs) {
   console.log(songs);
+  $('#songs').empty();
+  songs.forEach(function(song){
+    $('#songs').append('<li>' + song.title + ". By: " + song.artist + '. From: ' + song.album + '</li>')
+
+  });
 }
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var songs = require('./data.json');
-
 var app = express();
 
+//Makes everything in the public folder visible
 app.use(express.static('public'));
+//Coverts any URL Encoded body into a javascript object
+app.use(bodyParser.urlencoded({extended: true}));
 
+//Respond to get request with the full file name/path of index.html in the public folder
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-app.get('/data', function(req, res) {
+//Responds to get request for data with the songs array of objects
+app.get('/songs', function(req, res) {
   res.send(songs);
+});
+
+app.post('/songs', function(req, res){
+  console.log('req.body:', req.body);
+  songs.push(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(3000);
